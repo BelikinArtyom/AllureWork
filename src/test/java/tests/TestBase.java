528404e0eb5b329cbf1c237c63368b6be9b01968
Artyom Belikin <biloplus@yandex.ru>
2025-05-28 15:03:01 +0300

    секонд

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -4,11 +4,16 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.URL;
 import java.util.Map;
 
-public class TestBase {
+public class TestBase  {
+
+    protected static WebDriver driver;
 
     @BeforeAll
     public static void beforeAll() {
@@ -16,7 +21,9 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.headless = false;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        String selenoidUrl = System.getProperty("selenoidUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
@@ -24,6 +31,8 @@ public class TestBase {
                 "enableVideo", true
         ));
         Configuration.browserCapabilities = capabilities;
+
+        driver = new RemoteWebDriver(new URL(selenoidUrl), capabilities);
 
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
